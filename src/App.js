8f113f47a3b036917f874b5462ef4a82fb294490
Jeff Wang <wang4621@purdue.edu>
2022-09-25 22:36:22 -0400

    c

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="container">
+        <h1>Hello!</h1>
+      </div>
     </div>
   );
 }
