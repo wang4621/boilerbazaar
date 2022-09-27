ca44b5d87184537c975e212feb954f337ac371e3
Jeff Wang <wang4621@purdue.edu>
2022-09-26 21:37:33 -0400

    added icons for the different pages.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,43 @@ function App() {
       <div className="container">
         <div className="navBar">
           <img src={Img} height={50} alt="logo"></img>
-
+          <a href="#!">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+              <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+              <rect x="10" y="12" width="4" height="4"></rect>
+            </svg>
+          </a>
+          <a href="#!">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-store" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <line x1="3" y1="21" x2="21" y2="21"></line>
+              <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4"></path>
+              <line x1="5" y1="21" x2="5" y2="10.85"></line>
+              <line x1="19" y1="21" x2="19" y2="10.85"></line>
+              <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4"></path>
+            </svg>
+          </a>
+          <a href="#!">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <line x1="18" y1="6" x2="18" y2="6.01"></line>
+              <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5"></path>
+              <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15"></polyline>
+              <line x1="9" y1="4" x2="9" y2="17"></line>
+              <line x1="15" y1="15" x2="15" y2="20"></line>
+            </svg>
+          </a>
+          <a href="#!">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <circle cx="12" cy="12" r="9"></circle>
+              <circle cx="12" cy="10" r="3"></circle>
+              <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
+            </svg>
+          </a>
         </div>
-
       </div>
     </div>
   );
