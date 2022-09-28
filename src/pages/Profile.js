0c879ae4ac1ae7b0aa2db73b5593d6eb049d06ea
Jeff Wang <wang4621@purdue.edu>
2022-09-28 17:00:28 -0400

    c

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,29 +3,37 @@ import Box from '@mui/material/Box';
 
 function Profile () {
     return (
-        <form>
-            <label>First Name:</label>
-            <input type="text" disabled/>
-            <br/>
-            <label>Preferred Name:</label>
-            <input type="text"/>
-            <br/>
-            <label>Last Name:</label>
-            <input type="text" disabled/>
-            <br/>
-            <label>Major:</label>
-            <input type="text"/>
-            <br/>
-            <label>Preferred Meeting Location:</label>
-            <select>
-                <option value="public">Public</option>
-                <option value="front">In front of house</option>
-                <option selected value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-            </select>
-            <br/>
-            <input type="submit" value="Edit" />
-        </form>
+        <Box 
+            sx={{
+                width: 600,
+                height: 600,
+                backgroundColor: 'gray',
+            }}>
+            <div className="header">
+                <h1>Profile</h1>
+            </div>
+            <form>
+                <label>First Name:</label>
+                <input type="text" disabled/>
+                <br/>
+                <label>Preferred Name:</label>
+                <input type="text"/>
+                <br/>
+                <label>Last Name:</label>
+                <input type="text" disabled/>
+                <br/>
+                <label>Major:</label>
+                <input type="text"/>
+                <br/>
+                <label>Preferred Meeting Location:</label>
+                <select>
+                    <option value="public">Public</option>
+                    <option value="front">In front of house/apt</option>
+                </select>
+                <br/>
+                <input type="submit" value="Edit" />
+            </form>
+        </Box>
     )
 }
 export default Profile;
