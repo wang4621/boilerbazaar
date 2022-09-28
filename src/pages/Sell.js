0cbea2f346fcfa4cc40c499748b6e9ad32cd21d1
ryan-doan <82603138+ryan-doan@users.noreply.github.com>
2022-09-28 18:58:39 -0400

    added function to send listing to database

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -1,38 +1,55 @@
+function sendToDatabase() {
+    var title = document.getElementById("title");
+    var isbn = document.getElementById("isbn");
+    var author = document.getElementById("author");
+    var edition = document.getElementById("edition");
+    var price = document.getElementById("price");
+    var description = document.getElementById("description");
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "");
+    xhr.setRequestHeader("Request-type", "New Listing");
+
+    xhr.send(String.format("{'title': '%s', 'isbn': '%s', 'author': '%s',\
+    'edition': '%s', 'price': '%s', 'description': '%s',}", title, isbn,
+    author, edition, price, description));
+}
+
 function Sell () {
     return (
         <form>
             <label>
                 Title:
-                <input type="text" required/>
+                <input id="title" type="text" required/>
             </label>
             <br/>
             <label>
                 ISBN:
-                <input type="text" required/>
+                <input id="isbn" type="text" required/>
             </label>
             <br/>
             <label>
                 Author:
-                <input type="text" required/>
+                <input id="author" type="text" required/>
             </label>
             <br/>
             <label>
                 Edition:
-                <input type="text" required/>
+                <input id="edition" type="text" required/>
             </label>
             <br/>
             <label>
                 Price:
-                <input type="text" required/>
+                <input id="price" type="text" required/>
             </label>
             <br/>
             <label>
                 Description:
                 <br/>
-                <textarea maxlength="250"/>
+                <textarea id="description" maxlength="250"/>
             </label>
             <br/>
-            <input type="submit" value="Submit" />
+            <input type="submit" onClick="sendToDatabase()" value="Submit" />
         </form>
     )
 }
