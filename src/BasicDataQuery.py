b7f7696f6bede72202e24eeac65231160ed8fc91
ryan-doan <82603138+ryan-doan@users.noreply.github.com>
2022-09-28 16:55:47 -0400

    Format code for modularity

--- src/BasicDataQuery.py
+++ src/BasicDataQuery.py
@@ -1,10 +1,14 @@
 import pymongo
 
-databaseClient = pymongo.MongoClient("mongodb://localhost:27017")
+def databaseInit():
+    databaseClient = pymongo.MongoClient("mongodb://localhost:27017")
+    return databaseClient
+    
+
+databaseClient = databaseInit()
 mydb = databaseClient["BoilerBazaar"]
 myCollection = mydb["User"]
 
 query = myCollection.find({ "Major" : "Computer Science"})
-
 for x in query:
     print(x)
