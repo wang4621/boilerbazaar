a169432ff899cb44d20a34f91619e633c957f2d1
jeffreywang <wang4621@purdue.edu>
2022-09-28 09:37:33 -0400

    chnaged index

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout"
-import Home from "./pages/Home"
-import Buy from "./pages/Buy"
-import Sell from "./pages/Sell"
-import About from "./pages/About"
-import Map from "./pages/Map"
-import Profile from "./pages/Profile"
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="buy" element={<Buy />} />
-          <Route path="sell" element={<Sell />} />
-          <Route path="about" element={<About />} />
-          <Route path="map" element={<Map />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
