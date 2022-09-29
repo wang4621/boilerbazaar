08a205fbadc6133c2cac03c18bd25073bda86b6e
Jeff Wang <wang4621@purdue.edu>
2022-09-29 17:53:50 -0400

    c

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -1,20 +1,36 @@
-import './Sell.css'
+// import './Sell.css'
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card'
+// import Card from '@mui/material/Card'
+
+// adds dollar sign in front of price
+function handleDollar() {
+    var value = document.getElementById('price').value
+    if (value.includes('$')) {
+        value = value.split('$')[1]
+    }
+    if (isNaN(value) || value === '') {
+        document.getElementById('price').value = ''
+    } else {
+        value = "$" + value;
+        document.getElementById('price').value = value
+    }
+}
 
 function Sell () {
     return (
-        <div>
-            <Box sx={{width: '30%', height: '50 %', backgroundColor: 'gray'}}>
+        <div className="sellDisplay">
+            <Box sx={{width: '30%', height: '871px', backgroundColor: 'rgb(223, 223, 223)'}} className="formDisplay">
                 <form>
-                    <input type="text" placeholder="Title" font-size="50" required/>
-                    <br/>
+                    <span style={{"font-size": "25px", "font-weight": "bold"}}>Textbook For Sale</span>
+                    <br/><br/>
+                    <input type="text" placeholder="Title" required/>
+                    <br/><br/>
                     <input type="text" placeholder="ISBN" required/>
-                    <br/>
+                    <br/><br/>
                     <input type="text" placeholder="Author" required/>
-                    <br/>
+                    <br/><br/>
                     <input type="text" placeholder="Edition" required/>
-                    <br/>
+                    <br/><br/>
                     <select required> 
                         <option value="" disabled selected hidden>Condition</option>
                         <option value="new">New</option>
@@ -22,17 +38,14 @@ function Sell () {
                         <option value="good">Used - Good</option>
                         <option value="fair">Used - Fair</option>
                     </select>
-                    <br/>
-                    <div className="price">
-                        <input type="number" min="0" max="200" placeHolder="Price" required/>
-                    </div>
-                    <br/>
-                    <textarea maxlength="250" placeholder="Description" rows="4" cols="50"/>
-                    <br/>
-                    <input type="submit" value="List" />
+                    <br/><br/>
+                    <input type="text" id="price" placeholder="Price" onChange={handleDollar} maxLength="4" required/>
+                    <br/><br/>
+                    <textarea maxlength="250" placeholder="Description" rows="5" cols="30"/>
+                    <br/><br/>
+                    <input type="submit" value="List" id="list"/>
                 </form>
             </Box>
-            <Card variant="outlined"  sx={{ maxWidth: 345 }}>Hello</Card>
         </div>
     )
 }
