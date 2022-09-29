fd4bedf218ab7e09de64ccc27ee328a5990c16da
Jeff Wang <wang4621@purdue.edu>
2022-09-28 21:53:45 -0400

    c

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <div className="navbar">
             <img src={Img} height={70} alt="logo" style={{"float":"left"}}></img>
             <ul className="centerNav" style={{"margin-right":"15%"}}>
-              <Link to="/">Home</Link>
+              <Link to="/boilerbazaar">Home</Link>
               <Link to="/buy">Buy</Link>
               <Link to="/sell">Sell</Link>
               <Link to="/about">About</Link>
@@ -30,7 +30,7 @@ function App() {
               </Link>  
           </div>
           <Routes>
-            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/boilerbazaar' element={< Home />}></Route>
             <Route exact path='/buy' element={< Buy />}></Route>
             <Route exact path='/sell' element={< Sell />}></Route>
             <Route exact path='/about' element={< About />}></Route>
