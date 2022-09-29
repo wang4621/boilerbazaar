f2273cfc8315ed12675d399eeac080257880e892
x-pham <xpham@purdue.edu>
2022-09-29 15:01:44 -0400

    Fix spacing on map page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -2,6 +2,7 @@ function Map (){
     return (
         <div class="mapContainer">
             <h1 class = "mapTitle">Purdue Bookstore Locations</h1>
+            <hr class= "mapDivider"></hr>
             <h2 class = "bookstoreNames">University Bookstore</h2>
             <h2 class = "bookstoreNames">Stadium University Bookstore</h2>
             <h2 class = "bookstoreNames">Follett's Purdue West Bookstore</h2>
