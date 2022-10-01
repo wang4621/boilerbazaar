9bc1a4dc431e5f35b0bcfa1ee69b0ba74f35f858
Jeff Wang <wang4621@purdue.edu>
2022-09-30 21:46:47 -0400

    c

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -5,6 +5,8 @@ import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
 // adds dollar sign in front of price
 function handleDollar() {
@@ -23,65 +25,91 @@ function handleDollar() {
 function Sell () {
     return (
         <div className="sellDisplay">
-            <Box sx={{width: '25%', height: '100%', backgroundColor: 'rgb(223, 223, 223)'}} className="formDisplay">
-                <CardHeader title="Textbooks for sale"/>
-                <form>
-                    <br/><br/>
-                    <input type="text" placeholder="Title" required/>
-                    <br/><br/>
-                    <input type="text" placeholder="ISBN" required/>
-                    <br/><br/>
-                    <input type="text" placeholder="Author" required/>
-                    <br/><br/>
-                    <input type="text" placeholder="Edition" required/>
-                    <br/><br/>
-                    <select required> 
-                        <option value="" disabled selected hidden>Condition</option>
-                        <option value="new">New</option>
-                        <option value="likeNew">Used - Like New</option>
-                        <option value="good">Used - Good</option>
-                        <option value="fair">Used - Fair</option>
-                    </select>
-                    <br/><br/>
-                    <input type="text" id="price" placeholder="Price" onChange={handleDollar} maxLength="4" required/>
-                    <br/><br/>
-                    <textarea maxlength="250" placeholder="Description" rows="5" cols="30"/>
-                    <br/><br/>
-                    <input type="submit" value="List" id="list"/>
-                </form>
+            <Box sx={{width: '25%', height: '100%', backgroundColor: 'rgb(223, 223, 223)'}}>
+                <CardHeader title="Textbooks for sale" sx={{textAlign: 'center'}}/>
+                <div className="formDisplay">
+                    <form>
+                        <br/><br/>
+                        <input type="text" placeholder="Title" required/>
+                        <br/><br/>
+                        <input type="text" id="price" placeholder="Price" onChange={handleDollar} maxLength="4" required/>
+                        <br/><br/>
+                        <input type="text" placeholder="Author" required/>
+                        <br/><br/>
+                        <input type="text" placeholder="ISBN" required/>
+                        <br/><br/>
+                        <input type="text" placeholder="Edition" required/>
+                        <br/><br/>
+                        <select required> 
+                            <option value="" disabled selected hidden>Condition</option>
+                            <option value="new">New</option>
+                            <option value="likeNew">Used - Like New</option>
+                            <option value="good">Used - Good</option>
+                            <option value="fair">Used - Fair</option>
+                        </select>
+                        <br/><br/>
+                        <textarea maxlength="250" placeholder="Description" rows="5" cols="30"/>
+                        <br/><br/>
+                        <input type="submit" value="List" id="list"/>
+                    </form>
+                </div>
             </Box>
             <div className="previewDisplay">
                 <Card sx={{ width: "90%", borderRadius: 5, height: '90%'}} elevation={12}>
                         <CardHeader title="Preview"/>
                         <div className="innerCard">
-                            <Box sx={{ width: '60%', height: '88%', backgroundColor: 'gray'}} className="innerLeftBox">
+                            <Box sx={{ width: '58%', height: '88%', backgroundColor: 'rgb(223, 223, 223)'}} className="innerLeftBox">
                                 <CardContent sx={{alignItems: 'center', display: 'flex', justifyContent: 'center', height: '100%'}}>
-                                    <Typography variant="h4" color="white">
+                                    <Typography variant="h4" color="black">
                                         Listing Preview
                                     </Typography>
                                 </CardContent>
                             </Box>
-                            <Box sx={{ width: '35%', height: '88%', backgroundColor: 'rgb(223, 223, 223)'}} className="innerRightBox">
+                            <Box sx={{ width: '37%', height: '88%', backgroundColor: 'whitesmoke'}} className="innerRightBox">
                                 <CardContent>
-                                    <Typography variant="h5" color="black">
+                                    <Typography variant="h4" color="black" sx={{fontWeight:'bold'}}>
                                         Title
                                     </Typography>
-                                    <Typography variant="body1" color="black">
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
                                         Price
-                                        <br/>
+                                    </Typography>
+                                    <br/><br/>
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
                                         Details
+                                    </Typography>
+                                    <br/>
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
+                                        Author
                                         <br/>
+                                    </Typography>
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
+                                        ISBN
+                                        <br/>
+                                    </Typography>
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
+                                        Edition
+                                        <br/>
+                                    </Typography>               
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
+                                        Condition
+                                        <br/>
+                                    </Typography>
+                                    <br/>
+                                    <Typography variant="h6" color="black" sx={{fontWeight:'bold'}}>
                                         Description
                                     </Typography>
+                                    <br/><br/>
                                 </CardContent>
-                                <Divider variant='fullWidth'/>
+                                <Divider variant='middle' sx={{borderBottomColor: 'black'}}/>
                                 <CardContent>
-                                    <Typography variant="h6" color="black">
+                                    <Typography variant="body1" color="black" sx={{fontWeight:'bold', fontSize:18}}>
                                         Seller Information
+                                        <br/><br/>
                                     </Typography>
+                                    <Avatar></Avatar>
                                 </CardContent>
-                                <Box>
-
+                                <Box sx={{ width: '100%', height: '10%', backgroundColor: 'whitesmoke'}} className="innerBottomBox">
+                                    <Button variant="contained" disabled sx={{width: '95%', margin:'0'}}>Message</Button>
                                 </Box>
                             </Box>
                         </div>
