39986d8cebfa751cbf703996953adb71381dadb7
x-pham <xpham@purdue.edu>
2022-09-30 22:09:35 -0400

    Add hours to Map page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -6,8 +6,23 @@ function Map (){
             <h1 class = "mapTitle">Purdue Bookstore Locations</h1>
             <hr class= "mapDivider"></hr>
             <h2 class = "bookstoreNames">University Bookstore</h2>
+            <h3 class = "hoursHeader">Hours:</h3>
+            <p class = "hoursText">
+            Monday - Friday 9:00 AM - 5:30 PM<br/>
+            Saturday 10:00 AM - 5:00 PM<br/>
+            Sunday 12:00 PM - 5:00 PM</p>
             <h2 class = "bookstoreNames">Stadium University Bookstore</h2>
+            <h3 class = "hoursHeader">Hours:</h3>
+            <p class = "hoursText">
+            Monday - Friday 9:00 AM - 5:00 PM<br/>
+            Saturday 10:00 AM - 5:00 PM<br/>
+            Sunday Closed</p>
             <h2 class = "bookstoreNames">Follett's Purdue West Bookstore</h2>
+            <h3 class = "hoursHeader">Hours:</h3>
+            <p class = "hoursText">
+            Monday - Friday 9:00 AM - 5:00 PM<br/>
+            Saturday 10:00 AM - 5:00 PM<br/>
+            Sunday Closed</p>
         </div>
     )
 }
