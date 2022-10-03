07ad6dcc5bf620765f9f93af1be2d59f512e8cee
Michio Sekiguchi <sekiguchimichio02@gmail.com>
2022-10-02 23:20:42 -0400

    Added About UI structure

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,59 @@
 function About (){
     return (
-        <h1>About</h1>
+        <div>
+            <div>
+                <h1>About Boiler Bazaar</h1>
+                <p>about</p>
+            </div>
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                    <img img src="add image" alt="name" style="width:100%"/>
+                        <div class="container">
+                            <h2>name and info</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <img src="add image" alt="name" style="width:100%"/>
+                        <div class="container">
+                            <h2>name and info</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <img src="add image" alt="name" style="width:100%"/>
+                        <div class="container">
+                            <h2>name and info</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <img src="add image" alt="name" style="width:100%"/>
+                        <div class="container">
+                            <h2>name and info</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <img src="add image" alt="name" style="width:100%"/>
+                        <div class="container">
+                            <h2>name and info</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
     )
 }
   
