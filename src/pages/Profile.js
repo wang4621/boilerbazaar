686bb01452192b16c8480b3eb68e500de93bf94e
Jeff Wang <wang4621@purdue.edu>
2022-10-05 23:53:33 -0400

    added profile ui changes and alogrithm.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,50 +1,60 @@
 import './Profile.css'
-import { Avatar, Card, CardHeader, Rating, Divider, Box, Typography, CardContent, Button, TextField, MenuItem } from '@mui/material';
-
-function editOrSaveProfile(e) {
-    var isEdit = document.getElementById('editOrSave')
-    if (isEdit.value == "Edit") {
-        // enable inputs
-        document.getElementById('editOrSave').value = "Save"
-    } else if (isEdit.value == "Save") {
-        // send new values to server
-        document.getElementById('editOrSave').value = "Edit"
-    }
-    e.preventDefault()
-}
+import { Avatar, CardHeader, Rating, Divider, Box, Typography, CardContent, Button, TextField, MenuItem } from '@mui/material';
+import * as React from 'react';
 
 function Profile () {
+    const [isDisabled, setDisabled] = React.useState(true)
+    const [value, setValue] = React.useState('Edit')
+    const [location, setLocation] = React.useState('None');
+
+    const locationChange = event => {
+        console.log(event.target.value)
+        setLocation(event.target.value)
+    }
+
+    const editOrSaveProfile = event => {
+        setDisabled(!isDisabled)
+        if (value === "Edit") {
+            setValue('Save')
+        } else if (value === "Save") {
+            console.log(document.getElementById('preferredName').value)
+            console.log(document.getElementById('major').value)
+            console.log(location)
+            setValue('Edit')
+        }
+        event.preventDefault()
+    }
     return (
         <div className="profileDisplay">
-            <Card sx={{width: '85%', height: '90%', backgroundColor: 'white', borderRadius: 5, display: 'flex', flexDirection:'column'}} elevation={24}>
+            <Box sx={{width: '85%', height: '90%', backgroundColor: 'white', borderRadius: 5, display: 'flex', flexDirection:'column', boxShadow: 8}}>
                 <CardHeader title="Settings" sx={{textAlign: 'center', height: "5%"}}/>
                 <Divider variant='fullWidth' sx={{borderBottomColor: 'black'}}/>
                 <div className="profileBoxDisplay">
                     <Box sx={{width: '15%', height: '100%', backgroundColor: 'whitesmoke'}} className="diffSettings">
-                        <div className="buttonDisplay">
-                            <Button sx={{width: '100%'}} size="large">Profile</Button>
-                        </div>
+                        <Button sx={{width: '100%'}}>Profile</Button>
                     </Box>
                     <Box sx={{width: '35%', height: '100%', backgroundColor: 'whitesmoke', display:'flex', flexDirection:'column'}}>
                         <CardHeader sx={{textAlign:"center", height:'5%'}} title="General"/>
                         <Divider variant='middle' sx={{borderBottomColor: 'rgb(202, 199, 199)'}}/>
-                        <Box sx={{'& > :not(style)': { m: 1 }, height: "95%", overflowY: 'scroll'}} component="form" autoComplete="off" className="profileFormDisplay" onSubmit={event => editOrSaveProfile(event)}>
-                            <TextField id="" label="First Name" disabled/>
-                            <TextField id="" label="Preferred Name" disabled/>
-                            <TextField id="" label="Last Name" disabled/>
-                            <TextField id="" label="Major" disabled/>
-                            <TextField id="" name="" label="Preferred Meeting Location" select defaultValue="None" disabled>
+                        <br/>
+                        <Box sx={{'& > :not(style)': { m: 1.5 }, height: "95%", overflowY: 'auto'}} component="form" autoComplete="off" className="profileFormDisplay" onSubmit={editOrSaveProfile} id="profileForm">
+                            <TextField id="firstName" label="First Name" disabled value="Jeff"></TextField>
+                            <TextField id="preferredName" label="Preferred Name" disabled={isDisabled}/>
+                            <TextField id="lastName" label="Last Name" disabled value="Wang"/>
+                            <TextField id="major" label="Major" disabled={isDisabled}/>
+                            <TextField id="location" name="" label="Preferred Meeting Location" select value={location} disabled={isDisabled} onChange={locationChange}>
                                 <MenuItem value="None">None</MenuItem>
                                 <MenuItem value="Public">Public</MenuItem>
-                                <MenuItem value="Front">In front of house/apt</MenuItem>
+                                <MenuItem value="In front of house/apt">In front of house/apt</MenuItem>
                             </TextField>
-                            <TextField id="editOrSave" type="submit" value="Edit"/>
+                            <TextField type="submit" value={value}/>
                         </Box>
                     </Box>
                     <Box sx={{width: '50%', height: '100%', backgroundColor: 'whitesmoke'}} className="ratingBox">
-                        <Avatar sx={{ width: 128, height: 128 }}/>
-                        <Typography variant="h6" color="black">Profile Picture goes here</Typography>
-                        <Typography variant="h6" color="black">Name goes here</Typography>
+                        <Typography variant="h6" color="black" sx={{textAlign:'center'}}>
+                            <Avatar sx={{ width: 128, height: 128 }}/>
+                            Jeff Wang
+                        </Typography>
                         <br/>
                         <Typography variant="h6" color="black">Rating</Typography>
                         <Rating name="read-only" readOnly size="large"/>
@@ -65,7 +75,7 @@ function Profile () {
                         </Typography>
                     </Box>
                 </div>
-            </Card>
+            </Box>
         </div>
     )
 }
