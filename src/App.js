e99557f6367f4fee78665c62d0406656e6a53132
x-pham <xpham@purdue.edu>
2022-10-06 16:52:43 -0400

    Add dark mode button and fix spacing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { CgProfile } from 'react-icons/cg';
 import { TbMap2 } from 'react-icons/tb';
+import { MdOutlineDarkMode } from "react-icons/md";
 import './App.css';
 import Img from './logo.png'
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import Sell from './pages/Sell'
@@ -13,31 +14,36 @@ import Map from './pages/Map'
 function App() {
   return (
     <Router>
-        <div className="App">
-          <div className="navbar">
-            <img src={Img} height={70} alt="logo" style={{"float":"left"}}></img>
-            <ul className="centerNav" style={{"margin-right":"10%"}}>
-              <Link to="/">Home</Link>
-              <Link to="/buy">Buy</Link>
-              <Link to="/sell">Sell</Link>
-              <Link to="/about">About</Link>
-              <Link to="/map" style={{"margin-top":"5px"}}>
-                <TbMap2 size={30}></TbMap2>
-              </Link>
-            </ul>
-              <Link to="/profile" style={{"margin-top":"5px"}}>
-                <CgProfile size={30}></CgProfile>
-              </Link>  
-          </div>
-          <Routes>
-                <Route exact path='/' element={< Home />}></Route>
-                <Route exact path='/buy' element={< Buy />}></Route>
-                <Route exact path='/sell' element={< Sell />}></Route>
-                <Route exact path='/about' element={< About />}></Route>
-                <Route exact path='/map' element={< Map />}></Route>
-                <Route exact path='/profile' element={< Profile />}></Route>
-          </Routes>
-       </div>
+      <div className="App">
+        <div className="navbar">
+          <img src={Img} height={70} alt="logo" style={{ "float": "left" }}></img>
+          <ul className="centerNav" style={{ "margin-right": "10%" }}>
+            <Link to="/">Home</Link>
+            <Link to="/buy">Buy</Link>
+            <Link to="/sell">Sell</Link>
+            <Link to="/about">About</Link>
+            <Link to="/map" style={{ "margin-top": "5px" }}>
+              <TbMap2 size={30}></TbMap2>
+            </Link>
+          </ul>
+          <Link to="/profile" style={{ "margin-top": "5px" }}>
+            <CgProfile size={30}></CgProfile>
+          </Link>
+        </div>
+        <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/buy' element={< Buy />}></Route>
+          <Route exact path='/sell' element={< Sell />}></Route>
+          <Route exact path='/about' element={< About />}></Route>
+          <Route exact path='/map' element={< Map />}></Route>
+          <Route exact path='/profile' element={< Profile />}></Route>
+        </Routes>
+      </div>
+      <div class="d-flex justify-content-end">
+        <button style={{ "border": "none" }}>
+          <MdOutlineDarkMode size={40}></MdOutlineDarkMode>
+        </button>
+      </div>
     </Router>
   );
 }
