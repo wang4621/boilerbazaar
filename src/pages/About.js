934c4761848589d53e229b30810157a954676e3f
Michio Sekiguchi <sekiguchimichio02@gmail.com>
2022-10-09 19:14:40 -0400

    About css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,57 +1,79 @@
+import blank from './blank.jpg'
+import './About.css';
+
 function About (){
     return (
         <div>
-            <div>
+            <div  class="header">
                 <h1>About Boiler Bazaar</h1>
                 <p>about</p>
             </div>
-            <div class="row">
-                <div class="column">
-                    <div class="card">
-                    <img img src="add image" alt="name" style="width:100%"/>
+            <div class="horizontal">
+                <div class="vertical">
+                    <div class="member">
+                    <img src={blank} alt="name" 
+                    style={{
+                        width:100
+                    }}/>
                         <div class="container">
-                            <h2>name and info</h2>
+                            <h2>name</h2>
+                            <p>info</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="column">
-                    <div class="card">
-                        <img src="add image" alt="name" style="width:100%"/>
+                <div class="vertical">
+                    <div class="member">
+                    <img src={blank} alt="name" 
+                    style={{
+                        width:100
+                    }}/>
                         <div class="container">
-                            <h2>name and info</h2>
+                            <h2>name</h2>
+                            <p>info</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="column">
-                    <div class="card">
-                        <img src="add image" alt="name" style="width:100%"/>
+                <div class="vertical">
+                    <div class="member">
+                    <img src={blank} alt="name" 
+                    style={{
+                        width:100
+                    }}/>
                         <div class="container">
-                            <h2>name and info</h2>
+                            <h2>name</h2>
+                            <p>info</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="column">
-                    <div class="card">
-                        <img src="add image" alt="name" style="width:100%"/>
+                <div class="vertical">
+                    <div class="member">
+                    <img src={blank} alt="name" 
+                    style={{
+                        width:100
+                    }}/>
                         <div class="container">
-                            <h2>name and info</h2>
+                            <h2>name</h2>
+                            <p>info</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="column">
-                    <div class="card">
-                        <img src="add image" alt="name" style="width:100%"/>
+                <div class="vertical">
+                    <div class="member">
+                    <img src={blank} alt="name" 
+                    style={{
+                        width:100
+                    }}/>
                         <div class="container">
-                            <h2>name and info</h2>
+                            <h2>name</h2>
+                            <p>info</p>
                         </div>
                     </div>
                 </div>
             </div>
-            
         </div>
         
     )
