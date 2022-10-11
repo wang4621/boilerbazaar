4e3851750e504063f14167b83ac393b7b81ee67e
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-10-10 22:28:17 -0400

    added routing for different items in settings.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { TbMap2 } from 'react-icons/tb';
 import './App.css';
 import Img from './logo.png'
 import { BrowserRouter as Router,Routes, Route, NavLink } from 'react-router-dom';
-import Profile from './pages/Profile'
+import Settings from './pages/Settings'
 import Home from './pages/Home'
 import Sell from './pages/Sell'
 import Buy from './pages/Buy'
@@ -24,69 +24,69 @@ function App() {
   return (
     <Router>
         <div className="App">
-          <div className="navbar">
-            <img src={Img} height={70} alt="logo" style={{"float":"left"}}></img>
-            <ul className="centerNav" style={{"margin-right":"10%"}}>
-              <NavLink activeClassName="active" to="/boilerbazaar">Home</NavLink>
-              <NavLink activeClassName="active" to="/buy">Buy</NavLink>
-              <NavLink activeClassName="active" to="/sell">Sell</NavLink>
-              <NavLink activeClassName="active" to="/about">About</NavLink>
-              <NavLink activeClassName="active" to="/map" style={{"margin-top":"5px"}} id="map">
-                <TbMap2 size={28}></TbMap2>
-              </NavLink>
-            </ul>
-            <IconButton onClick={handleClick} size="large" aria-controls={open ? 'account-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined}>
-              <Avatar sx={{ width: 35, height: 35 }} src=""/>
-            </IconButton>
-            <Menu anchorEl={anchorEl} id="account-menu" open={open} onClose={handleClose} onClick={handleClose}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: 'visible',
-                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                  mt: 0,
-                  '& .MuiAvatar-root': {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  '&:before': {
-                    content: '""',
-                    display: 'block',
-                    position: 'absolute',
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: 'background.paper',
-                    transform: 'translateY(-50%) rotate(45deg)',
-                    zIndex: 0,
-                  },
-                },
-              }}
-              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
-              <MenuItem component={NavLink} to="/profile">
-                  <Avatar src=""/> Profile
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <Logout fontSize="small" />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </div>
-          <Routes>
-            <Route exact path='/boilerbazaar' element={< Home />}></Route>
-            <Route exact path='/buy' element={< Buy />}></Route>
-            <Route exact path='/sell' element={< Sell />}></Route>
-            <Route exact path='/about' element={< About />}></Route>
-            <Route exact path='/map' element={< Map />}></Route>
-            <Route exact path='/profile' element={< Profile />}></Route>
-          </Routes>
-       </div>
+            <div className="navbar">
+                <img src={Img} height={70} alt="logo" style={{"float":"left"}}></img>
+                <ul className="centerNav" style={{"margin-right":"10%"}}>
+                    <NavLink activeClassName="active" to="/boilerbazaar">Home</NavLink>
+                    <NavLink activeClassName="active" to="/buy">Buy</NavLink>
+                    <NavLink activeClassName="active" to="/sell">Sell</NavLink>
+                    <NavLink activeClassName="active" to="/about">About</NavLink>
+                    <NavLink activeClassName="active" to="/map" style={{"margin-top":"5px"}} id="map">
+                        <TbMap2 size={28}></TbMap2>
+                    </NavLink>
+                </ul>
+                <IconButton onClick={handleClick} size="large" aria-controls={open ? 'account-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined}>
+                    <Avatar sx={{ width: 35, height: 35 }} src=""/>
+                </IconButton>
+                <Menu anchorEl={anchorEl} id="account-menu" open={open} onClose={handleClose} onClick={handleClose}
+                    PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 0,
+                        '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                        },
+                        '&:before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                        },
+                    },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+                    <MenuItem component={NavLink} to="/settings/display">
+                        <Avatar src=""/> Settings
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </div>
+            <Routes>
+                <Route path='/boilerbazaar' element={< Home />}/>
+                <Route path='/buy' element={< Buy />}/>
+                <Route path='/sell' element={< Sell />}/>
+                <Route path='/about' element={< About />}/>
+                <Route path='/map' element={< Map />}/>
+                <Route path='/settings/*' element={< Settings />}/>
+            </Routes>
+        </div>
     </Router>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,13 @@
-import './Profile.css'
-import { Avatar, CardHeader, Rating, Divider, Box, Typography, CardContent, Button, TextField, MenuItem } from '@mui/material';
+import { Avatar, CardHeader, Rating, Divider, Box, Typography, Button, TextField, MenuItem } from '@mui/material';
 import * as React from 'react';
+import './Profile.css'
 
-function Profile () {
+function Profile() {
     const [isDisabled, setDisabled] = React.useState(true)
     const [value, setValue] = React.useState('Edit')
     const [location, setLocation] = React.useState('None');
 
     const locationChange = event => {
-        console.log(event.target.value)
         setLocation(event.target.value)
     }
 
@@ -24,60 +23,53 @@ function Profile () {
         }
         event.preventDefault()
     }
+
     return (
-        <div className="profileDisplay">
-            <Box sx={{width: '85%', height: '90%', backgroundColor: 'white', borderRadius: 5, display: 'flex', flexDirection:'column', boxShadow: 8}}>
-                <CardHeader title="Settings" sx={{textAlign: 'center', height: "5%"}}/>
-                <Divider variant='fullWidth' sx={{borderBottomColor: 'black'}}/>
-                <div className="profileBoxDisplay">
-                    <Box sx={{width: '15%', height: '100%', backgroundColor: 'whitesmoke'}} className="diffSettings">
-                        <Button sx={{width: '100%'}}>Profile</Button>
-                    </Box>
-                    <Box sx={{width: '35%', height: '100%', backgroundColor: 'whitesmoke', display:'flex', flexDirection:'column'}}>
-                        <CardHeader sx={{textAlign:"center", height:'5%'}} title="General"/>
-                        <Divider variant='middle' sx={{borderBottomColor: 'rgb(202, 199, 199)'}}/>
-                        <br/>
-                        <Box sx={{'& > :not(style)': { m: 1.5 }, height: "95%", overflowY: 'auto'}} component="form" autoComplete="off" className="profileFormDisplay" onSubmit={editOrSaveProfile} id="profileForm">
-                            <TextField id="firstName" label="First Name" disabled value="Jeff"></TextField>
-                            <TextField id="preferredName" label="Preferred Name" disabled={isDisabled}/>
-                            <TextField id="lastName" label="Last Name" disabled value="Wang"/>
-                            <TextField id="puid" label="PUID" disabled value="0031888129"/>
-                            <TextField id="major" label="Major" disabled={isDisabled}/>
-                            <TextField id="location" label="Preferred Meeting Location" select value={location} disabled={isDisabled} onChange={locationChange}>
-                                <MenuItem value="None">None</MenuItem>
-                                <MenuItem value="Public">Public</MenuItem>
-                                <MenuItem value="In front of house/apt">In front of house/apt</MenuItem>
-                            </TextField>
-                            <TextField type="submit" value={value}/>
-                        </Box>
-                    </Box>
-                    <Box sx={{width: '50%', height: '100%', backgroundColor: 'whitesmoke'}} className="ratingBox">
-                        <Typography variant="h6" color="black" sx={{textAlign:'center'}}>
-                            <Avatar sx={{ width: 128, height: 128 }}/>
-                            Jeff Wang
-                        </Typography>
-                        <br/>
-                        <Typography variant="h6" color="black">Rating</Typography>
-                        <Rating name="read-only" readOnly size="large"/>
-                        <br/><br/>
-                        <Typography variant="h6" color="black">
-                            Completed Purchases:
-                            <Typography display="inline">&nbsp;</Typography>
-                            <Typography variant="h6" color="black" display="inline" id="purchases">
-                                5
-                            </Typography>
-                        </Typography>
-                        <Typography variant="h6" color="black">
-                            Completed Sales:
-                            <Typography display="inline">&nbsp;</Typography>
-                            <Typography variant="h6" color="black" display="inline" id="sales">
-                                1
-                            </Typography>
-                        </Typography>
-                    </Box>
-                </div>
+        <Box sx={{width: '85%', height: '100%', display: 'flex', flexDirection:'row'}}>
+            <Box sx={{width: '35%', height: '100%', backgroundColor: 'whitesmoke', display:'flex', flexDirection:'column'}}>
+                <CardHeader sx={{textAlign:"center", height:'5%'}} title="General"/>
+                <Divider variant='middle' sx={{borderBottomColor: 'rgb(202, 199, 199)'}}/>
+                <br/>
+                <Box sx={{'& > :not(style)': { m: 1.5 }, height: "95%", overflowY: 'auto'}} component="form" autoComplete="off" className="profileFormDisplay" onSubmit={editOrSaveProfile} id="profileForm">
+                    <TextField id="firstName" label="First Name" disabled value="Jeff"></TextField>
+                    <TextField id="preferredName" label="Preferred Name" disabled={isDisabled}/>
+                    <TextField id="lastName" label="Last Name" disabled value="Wang"/>
+                    <TextField id="puid" label="PUID" disabled value="0031888129"/>
+                    <TextField id="major" label="Major" disabled={isDisabled}/>
+                    <TextField id="location" label="Preferred Meeting Location" select value={location} disabled={isDisabled} onChange={locationChange}>
+                        <MenuItem value="None">None</MenuItem>
+                        <MenuItem value="Public">Public</MenuItem>
+                        <MenuItem value="In front of house/apt">In front of house/apt</MenuItem>
+                    </TextField>
+                    <TextField type="submit" value={value}/>
+                </Box>
             </Box>
-        </div>
+            <Box sx={{width: '65%', height: '100%', backgroundColor: 'whitesmoke'}} className="ratingBox">
+                <Typography variant="h6" color="black" sx={{textAlign:'center'}}>
+                    <Avatar sx={{ width: 128, height: 128 }}/>
+                    Jeff Wang
+                </Typography>
+                <br/>
+                <Typography variant="h6" color="black">Rating</Typography>
+                <Rating name="read-only" readOnly size="large"/>
+                <br/><br/>
+                <Typography variant="h6" color="black">
+                    Completed Purchases:
+                    <Typography display="inline">&nbsp;</Typography>
+                    <Typography variant="h6" color="black" display="inline" id="purchases">
+                        5
+                    </Typography>
+                </Typography>
+                <Typography variant="h6" color="black">
+                    Completed Sales:
+                    <Typography display="inline">&nbsp;</Typography>
+                    <Typography variant="h6" color="black" display="inline" id="sales">
+                        1
+                    </Typography>
+                </Typography>
+            </Box>       
+        </Box>
     )
 }
+
 export default Profile;
