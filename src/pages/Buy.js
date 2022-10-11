e3067172e2cc4193cdd156b4b5dfcd6c818bb6cb
x-pham <xpham@purdue.edu>
2022-10-11 13:30:49 -0400

    Add toggle filter display to Buy

--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -1,6 +1,25 @@
+import './Buy.css';
+
 function Buy (){
+    //Function to toggle the display of filters and sorting options
+    function toggleFilters() {
+        const filtersDiv = document.getElementById("filters");
+        const filtersButton = document.getElementById("filtersButton");
+        if (filtersDiv.style.display !== "none") {
+            filtersDiv.style.display = "none";
+        }
+        else {
+            filtersDiv.style.display = "block";
+        }
+    }
     return (
-        <h1>Buy</h1>
+        <div>
+            <h1>Buy</h1>
+            <button id="filtersButton" onClick={toggleFilters}>Show Filters and Sorting</button>
+            <div class="filters" id="filters">
+                <input type="checkbox"></input>
+            </div>
+        </div>
     )
 }
   
