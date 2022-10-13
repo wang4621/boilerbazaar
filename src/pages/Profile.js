7b33e90879d40b5671356ad25b0a3416ece15f6c
x-pham <xpham@purdue.edu>
2022-10-13 15:38:04 -0400

    Change some text colors on Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import { Avatar, CardHeader, Rating, Divider, Box, Typography, TextField, MenuIt
 import * as React from 'react';
 import './Profile.css'
 import $ from 'jquery';
+import { styled  } from "@mui/material/styles";
 
 function Profile() {
     const [isDisabled, setDisabled] = React.useState(true)
@@ -43,23 +44,30 @@ function Profile() {
         event.preventDefault()
     }
 
+    const CustomDisabledTextField = styled (TextField)(() => ({
+        ".MuiInputBase-input.Mui-disabled": {
+          WebkitTextFillColor: "var(--text-color)",
+          color: "var(--text-color)"
+        }
+      }));
+
     return (
-        <Box sx={{width: '85%', height: '100%', display: 'flex', flexDirection:'row' }}>
+        <Box className="profileDisplay">
             <Box sx={{width: '35%', height: '100%', backgroundColor: 'var(--secondary-color)', display:'flex', flexDirection:'column'}}>
                 <CardHeader sx={{textAlign:"center", height:'5%'}} title="General"/>
                 <Divider variant='middle' sx={{borderBottomColor: 'rgb(202, 199, 199)'}}/>
                 <br/>
                 <Box sx={{'& > :not(style)': { m: 1.5 }, height: "95%", overflowY: 'auto'}} component="form" autoComplete="off" className="profileFormDisplay" onSubmit={editOrSaveProfile} id="profileForm">
-                    <TextField id="firstName" label="First Name" disabled value="Jeff"></TextField>
-                    <TextField id="preferredName" label="Preferred Name" disabled={isDisabled} value={preferredName}/>
-                    <TextField id="lastName" label="Last Name" disabled value={lastName}/>
-                    <TextField id="puid" label="PUID" disabled value="0031888129"/>
-                    <TextField id="major" label="Major" disabled={isDisabled}/>
-                    <TextField id="location" label="Preferred Meeting Location" select value={location} disabled={isDisabled} onChange={locationChange}>
+                    <CustomDisabledTextField id="firstName" label="First Name" disabled value="Jeff"/>
+                    <CustomDisabledTextField id="preferredName" label="Preferred Name" disabled={isDisabled} value={preferredName}/>
+                    <CustomDisabledTextField id="lastName" label="Last Name" disabled value={lastName}/>
+                    <CustomDisabledTextField id="puid" label="PUID" disabled value="0031888129"/>
+                    <CustomDisabledTextField id="major" label="Major" disabled={isDisabled}/>
+                    <CustomDisabledTextField id="location" label="Preferred Meeting Location" select value={location} disabled={isDisabled} onChange={locationChange}>
                         <MenuItem value="None">None</MenuItem>
                         <MenuItem value="Public">Public</MenuItem>
                         <MenuItem value="In front of house/apt">In front of house/apt</MenuItem>
-                    </TextField>
+                    </CustomDisabledTextField>
                     <TextField type="submit" value={value}/>
                 </Box>
             </Box>
