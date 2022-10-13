223bfcd6cbdd72b22547cf07614384e1b70bc28e
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-10-13 19:31:03 -0400

    added routing for buy page

---
+++ src/pages/singleListing.js
@@ -0,0 +1,14 @@
+import './SingleListing.css'
+import * as React from 'react';
+import { Avatar, CardHeader, Rating, Divider, Box, Typography, TextField, MenuItem } from '@mui/material';
+
+function SingleListing() {
+    return (
+        <div className="listingDisplay">
+            <Typography variant="h6" color="black">
+                Listing
+            </Typography>
+        </div>
+    )
+}
+export default SingleListing;
