3f23e8d1855e81c7263f63ebbaa4f252e1a85601
ryan-doan <82603138+ryan-doan@users.noreply.github.com>
2022-10-14 13:36:25 -0400

    update about page with emails and links to github

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,6 +13,18 @@ function About (){
             <div class="horizontal">
                 <div class="vertical">
                     <div class="member">
+                        <div class="container">
+                            <h2>Project Repository</h2>
+                            <p>
+                                <IconButton sx={{ ml: 1, transform: 'scale(2)' }} target="_blank" href="https://github.com/wang4621/boilerbazaar" rel="noopener noreferrer">
+                                    <GitHubIcon/>
+                                </IconButton>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="vertical">
+                    <div class="member">
                     <img src={blank} alt="name" 
                     style={{
                         width:100
@@ -21,7 +33,7 @@ function About (){
                             <h2>Jeffrey Wang</h2>
                             <p>Contact Info: wang4621@purdue.edu</p>
                             <p>
-                                <IconButton target="_blank" href="www.google.com" rel="noopener noreferrer">
+                                <IconButton target="_blank" href="https://github.com/wang4621" rel="noopener noreferrer">
                                     <GitHubIcon/>
                                 </IconButton>
                             </p>
@@ -36,8 +48,13 @@ function About (){
                         width:100
                     }}/>
                         <div class="container">
-                            <h2>name</h2>
-                            <p>info</p>
+                            <h2>Michio L Sekiguchi</h2>
+                            <p>Contact Info: msekiguc@purdue.edu</p>
+                            <p>
+                                <IconButton target="_blank" href="https://github.com/msekiguc" rel="noopener noreferrer">
+                                    <GitHubIcon/>
+                                </IconButton>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -49,8 +66,13 @@ function About (){
                         width:100
                     }}/>
                         <div class="container">
-                            <h2>name</h2>
-                            <p>info</p>
+                            <h2>Ryan Doan</h2>
+                            <p>Contact Info: doan23@purdue.edu</p>
+                            <p>
+                                <IconButton target="_blank" href="https://github.com/ryan-doan" rel="noopener noreferrer">
+                                    <GitHubIcon/>
+                                </IconButton>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -62,8 +84,13 @@ function About (){
                         width:100
                     }}/>
                         <div class="container">
-                            <h2>name</h2>
-                            <p>info</p>
+                            <h2>Xavier Huu Pham</h2>
+                            <p>Contact Info: xpham@purdue.edu</p>
+                            <p>
+                                <IconButton target="_blank" href="https://github.com/x-pham" rel="noopener noreferrer">
+                                    <GitHubIcon/>
+                                </IconButton>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -75,8 +102,13 @@ function About (){
                         width:100
                     }}/>
                         <div class="container">
-                            <h2>name</h2>
-                            <p>info</p>
+                            <h2>Shicheng Fang</h2>
+                            <p>Contact Info: fang282@purdue.edu</p>
+                            <p>
+                                <IconButton target="_blank" href="https://github.com/fsc1118" rel="noopener noreferrer">
+                                    <GitHubIcon/>
+                                </IconButton>
+                            </p>
                         </div>
                     </div>
                 </div>
