2713c25b085c2bd0af845c9dfc1cb2400f192f06
jeffreywang <wang4621@purdue.edu>
2022-10-19 14:47:44 -0400

    added ajax call to get user textbook listings

--- src/pages/Listings.js
+++ src/pages/Listings.js
@@ -1,14 +1,29 @@
-import { Avatar, CardHeader, Rating, Divider, Box, Typography, Button, TextField, MenuItem } from '@mui/material';
-import * as React from 'react';
+import { Divider, Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import $ from 'jquery';
 
 function Listings() {
+    useEffect(() => {
+        // get user textbook listings
+        $.ajax({
+            url: 'https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?puid=0031888129',
+            type: 'GET',
+            success: function (result) {
+                console.log(result.length)
+                console.log(JSON.stringify(result));
+            },
+            error: function (result) {
+                console.log(JSON.stringify(result));
+            }
+          });
+    }, [])
+
     return (
-        <Box sx={{width: '85%', height: '100%', display: 'flex', flexDirection:'row'}}>
-            <Box sx={{width: '100%', height: '100%', display: 'flex', justifyContent:'center', alignItems:'center'}}>
-                <Typography variant="h6" color="var(--text-color)">
-                    You have no listings
-                </Typography>
-            </Box>
+        <Box sx={{width: '80%', height: '100%', display: 'flex', flexDirection:'column'}}>
+            <Typography variant="h6" sx={{fontWeight:'bold', textAlign:'center', padding:'10px'}}>
+                Your Listing
+            </Typography>
+            <Divider variant='middle' sx={{borderBottomColor: 'var(--text-color)'}}/>
         </Box>
     )
 }
