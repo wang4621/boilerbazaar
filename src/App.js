52bb6c70d7869caefdd6b46100d8dd35a8b294e5
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-10-20 17:43:48 -0400

    c

--- src/App.js
+++ src/App.js
@@ -69,9 +69,8 @@ function App() {
       url: 'https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/profile?puid=0031888129',
       type: 'GET',
       success: function (result) {
-        let returnedItem = result.Item;
-        window.localStorage.setItem('userData', JSON.stringify(returnedItem))
-        if (returnedItem.darkModePreference === 'dark') {
+        localStorage.setItem('userData', JSON.stringify(result))
+        if (result.darkModePreference === 'dark') {
           setTheme('bodyDark');
           root.style.setProperty('--primary-color', "#1e252e");
           root.style.setProperty('--secondary-color', "#323d4d");
