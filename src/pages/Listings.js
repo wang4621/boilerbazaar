9941dee5b42c55918904358f89b9b55d9e363c00
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-10-19 23:46:41 -0400

    added changeicon function to listing page

--- src/pages/Listings.js
+++ src/pages/Listings.js
@@ -1,17 +1,63 @@
-import { Divider, Box, Typography } from '@mui/material';
+import { Divider, Box, Typography, Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import $ from 'jquery';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Listings() {
     const [listedTextbooks, setListedTextbooks] = useState(<CircularProgress/>);
+    const [soldOrAvailable, setSoldOrAvailable] = useState("Mark as Sold");
+    const [soldOrAvailableIcon, setSoldOrAvailableIcon] = useState(<CloseIcon/>);
+    
+    function changeIcon() {
+        console.log(soldOrAvailable)
+        if (soldOrAvailable === "Mark as Available") {
+            setSoldOrAvailable("Mark as Sold")
+            setSoldOrAvailableIcon(<CloseIcon/>)
+        } else {
+            setSoldOrAvailable("Mark as Available")
+            setSoldOrAvailableIcon(<CheckIcon/>)
+        }
+    }
+
     function generateListingUI(listing) {
         console.log(listing)
         return (
-            <Box m={2} pt={1} sx={{width: '50%', height: '20%', display: 'flex', flexDirection:'column', backgroundColor:'var(--secondary-color)', borderRadius:5, boxShadow: 8}}>
-                <Typography variant='h5'>
-                    {listing['title']}
-                </Typography>
+            <Box m={2} sx={{width: '60%', height: '20%', display: 'flex', flexDirection:'row', justifyContent: 'center', backgroundColor:'var(--secondary-color)', borderRadius:5, boxShadow: 8}}>
+                <Box sx={{height:'100%', width:'30%', display: 'flex', alignItems:'center', justifyContent:'center'}}>
+                    <Box sx={{height:'80%', width:'80%', backgroundColor:'lightgrey', borderRadius:5, display:'flex', justifyContent:'center', alignItems:'center'}}>
+                        Image Preview
+                    </Box>
+                </Box>
+                <Box sx={{height:'100%', width:'70%', display:'flex', justifyContent:'flex-start', alignItems:'center'}}>
+                    <Box sx={{height: '80%', width: '95%'}}>
+                        <Box sx={{height: '80%', width: '100%'}}>
+                            <Typography variant='body1' sx={{fontWeight:'bold'}}>
+                                {listing['title']}
+                            </Typography>
+                            <Typography variant='body1'>
+                                ${listing['price']}
+                            </Typography>
+                            <Typography variant='body2'>
+                                Listed on 
+                            </Typography>
+                        </Box>
+                        <Box sx={{height: '20%', width: '100%', display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                            <Button variant='contained' startIcon={soldOrAvailableIcon} sx={{height:'100% !important', width: '35%', borderRadius: '5px !important'}} onClick={changeIcon}>
+                                {soldOrAvailable}
+                            </Button>
+                            <Button variant='contained' startIcon={<EditIcon/>} sx={{height:'100% !important', width: '30%', borderRadius: '5px !important'}}>
+                                Edit Listing
+                            </Button>
+                            <Button variant='contained' startIcon={<DeleteIcon/>} sx={{height:'100% !important', width: '30%', borderRadius: '5px !important'}}>
+                                Delete Listing
+                            </Button>
+                        </Box>
+                    </Box>
+                </Box>
             </Box>
         );
     } 
@@ -49,13 +95,13 @@ function Listings() {
 
     return (
         <Box sx={{width: '80%', height: '100%', display: 'flex', flexDirection:'column'}}>
-            <Box sx={{height: '6%'}}>
+            <Box sx={{height: '8%'}}>
                 <Typography variant='h6' sx={{fontWeight:'bold', textAlign:'center', padding:'10px'}}>
                     Your Listings
                 </Typography>
                 <Divider variant='middle' sx={{borderBottomColor: 'var(--text-color)'}}/>
             </Box>
-            <Box sx={{height: '94%', display: 'flex', flexDirection:'column', alignItems:'center'}}>
+            <Box sx={{height: '94%', display: 'flex', flexDirection:'column', alignItems:'center', overFlowY:'scroll'}}>
                 {listedTextbooks}
             </Box>
         </Box>
