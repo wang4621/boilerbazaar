bac06a4f7e0a9cb68eb676605a3650dd9a194726
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-10-21 15:34:30 -0400

    added changing icon functionality, added ajax calls

--- src/pages/Listings.js
+++ src/pages/Listings.js
@@ -1,5 +1,5 @@
-import { Divider, Box, Typography, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from '@mui/material';
-import { AppBar, Toolbar, IconButton, ListItemText, ListItem, List, Fade } from '@mui/material';
+import { Divider, Box, Typography, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { AppBar, Toolbar, IconButton, ListItemText, ListItem, List } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import $ from 'jquery';
@@ -10,41 +10,39 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 function Listings() {
     const [listedTextbooks, setListedTextbooks] = useState(<CircularProgress/>);
-    // const [soldOrAvailable, setSoldOrAvailable] = useState({});
-    // const [soldOrAvailableIcon, setSoldOrAvailableIcon] = useState({});
     const [sold, setSold] = React.useState({})
     const [deleteOpen, setDeleteOpen] = React.useState(false);
     const [editOpen, setEditOpen] = React.useState(false);
     const [listingID, setListingID] = React.useState('');
 
-    const handleClickOpen = (listingId) => {
+    const openDelete = (listingId) => {
         setDeleteOpen(true);
-        setListingID(listingID);
-        // setOpen(open => ({
-        //     ...open,
-        //     [listingID]: true
-        // }))
+        setListingID(listingId);
     };
     
-    const handleClose = () => {
+    const deleteClose = () => {
         setDeleteOpen(false);
-        // setOpen(open => ({
-        //     ...open,
-        //     [listingID]: false
-        // }))
     };
 
-    const deleteListing = event => {
-        console.log(event)
+    const deleteListing = () => {
+        console.log(listingID)
+        $.ajax({
+            url: 'https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/listing?listingID=' + listingID,
+            type: 'DELETE',
+            success: function (result) {
+                alert(JSON.stringify(result))
+                setDeleteOpen(false)
+                setListingID('')
+            },
+            error: function (result) {
+                console.log(JSON.stringify(result));
+            }
+        });
     }
 
     const openEdit = (listingId) => {
         setEditOpen(true);
-        setListingID(listingID);
-        // setOpen(open => ({
-        //     ...open,
-        //     [listingID]: true
-        // }))
+        setListingID(listingId);
     };
 
     const closeEdit = () => {
@@ -52,78 +50,49 @@ function Listings() {
     }
     
     // on button click for "Mark as Sold" and "Mark as Available"
-    const changeTextAndIcon = (listingID, event) => {
-        console.log(listingID)
-        console.log(event)
-        if (event.target.innerText === 'Mark as Sold') {
-            event.target.innerText = 'Mark as Available';
-            // send ajax to update, on success - setSold
-            setSold(sold => ({
-                ...sold,
-                [listingID]: true
-            }))
-        } else {
-            event.target.innerText = 'Mark as Sold'
-            // send ajax to update, on sucess - setSold
-            setSold(sold => ({
-                ...sold,
-                [listingID]: false
-            }))
-        }
-
+    const changeTextAndIcon = (listingId, event) => {
+        console.log(listingId)
+        // console.log(event)
         // console.log(sold)
-        // console.log(<CloseIcon></CloseIcon>)
-        // event.target.innerText = "Mark as Available"
-        // if (soldOrAvailable[listingID] === 'Mark as Sold') {
-        //     setSoldOrAvailable(soldOrAvailable => ({
-        //         ...soldOrAvailable,
-        //         [listingID]: 'Mark as Available'
-        //     }))
-        //     setSoldOrAvailableIcon(soldOrAvailableIcon => ({
-        //         ...soldOrAvailableIcon,
-        //         [listingID]: <CheckIcon/>
-        //     }))
-        // } else {
-        //     setSoldOrAvailable(soldOrAvailable => ({
-        //         ...soldOrAvailable,
-        //         [listingID]: 'Mark as Sold'
-        //     }))
-        //     setSoldOrAvailableIcon(soldOrAvailableIcon => ({
-        //         ...soldOrAvailableIcon,
-        //         [listingID]: <CloseIcon/>
-        //     }))
-        // }
+        if (event.target.innerText === 'Mark as Sold') {
+            // send ajax to update, on success - setSold
+            $.ajax({
+                url: 'https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?listingID=' + listingId + '&sold=true',
+                type: 'PUT',
+                success: function (result) {
+                    console.log(JSON.stringify(result));
+                    setSold(sold => ({
+                        ...sold,
+                        [listingId]: true
+                    }))
+                },
+                error: function (result) {
+                    console.log(JSON.stringify(result));
+                }
+            });
+        } else {
+            // send ajax to update, on sucess - setSold
+            $.ajax({
+                url: 'https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?listingID=' + listingId + '&sold=false',
+                type: 'PUT',
+                success: function (result) {
+                    console.log(JSON.stringify(result));
+                    setSold(sold => ({
+                        ...sold,
+                        [listingId]: false
+                    }))
+                },
+                error: function (result) {
+                    console.log(JSON.stringify(result));
+                }
+            });
+        }
     }
 
-    // function setListingValues(listing) {
-    //     console.log(listing)
-    //     let listingID = listing['listingID'];
-    //     if (!listing['sold']) {
-    //         setSoldOrAvailable(soldOrAvailable => ({
-    //             ...soldOrAvailable,
-    //             [listingID]: 'Mark as Sold'
-    //         }))
-    //         setSoldOrAvailableIcon(soldOrAvailableIcon => ({
-    //             ...soldOrAvailableIcon,
-    //             [listingID]: <CloseIcon/>
-    //         }))
-    //     } else {
-    //         setSoldOrAvailable(soldOrAvailable => ({
-    //             ...soldOrAvailable,
-    //             [listingID]: 'Mark as Available'
-    //         }))
-    //         setSoldOrAvailableIcon(soldOrAvailableIcon => ({
-    //             ...soldOrAvailableIcon,
-    //             [listingID]: <CheckIcon/>
-    //         }))
-    //     }
-    // }
-
     function generateListingUI(listing) {
-        // console.log(listing)
-        
         let listingSold = listing['sold'];
         let listingId = listing['listingID'];
+        console.log("listingID:", listingId + " sold: ",listingSold)
         return (
             <Box key={listingId} m={2} sx={{width: '80%', height: '20%', display: 'flex', flexDirection:'row', justifyContent: 'center', backgroundColor:'var(--secondary-color)', borderRadius:5, boxShadow: 8}}>
                 <Box sx={{height:'100%', width:'30%', display: 'flex', alignItems:'center', justifyContent:'center'}}>
@@ -148,13 +117,13 @@ function Listings() {
                             </Typography>
                         </Box>
                         <Box sx={{height: '20%', width: '100%', display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
-                            <Button variant='contained' startIcon={listingSold ? <CheckIcon/> : <CloseIcon/>} sx={{height:'100% !important', width: '35%', borderRadius: '5px !important'}} onClick={event => changeTextAndIcon(listingId, event, listingSold)}>
-                                {listingSold ? 'Mark as Available' : 'Mark as Sold'}
+                            <Button variant='contained' startIcon={listingSold=='true' ? <CheckIcon/> : <CloseIcon/>} sx={{height:'100% !important', width: '35%', borderRadius: '5px !important'}} onClick={event => changeTextAndIcon(listingId, event)}>
+                                {listingSold=='true' ? 'Mark as Available' : 'Mark as Sold'}
                             </Button>
                             <Button variant='contained' startIcon={<EditIcon/>} sx={{height:'100% !important', width: '30%', borderRadius: '5px !important'}} onClick={() => openEdit(listingId)}>
                                 Edit Listing
                             </Button>
-                            <Button variant='contained' startIcon={<DeleteIcon/>} sx={{height:'100% !important', width: '30%', borderRadius: '5px !important'}} onClick={() => handleClickOpen(listingId)}>
+                            <Button variant='contained' startIcon={<DeleteIcon/>} sx={{height:'100% !important', width: '30%', borderRadius: '5px !important'}} onClick={() => openDelete(listingId)}>
                                 Delete Listing
                             </Button>
                         </Box>
@@ -173,15 +142,6 @@ function Listings() {
         );
     }
 
-    // useEffect(() => {
-    //     var listingResults = JSON.parse(localStorage.getItem('userListing'))
-    //     let textBooks = []
-    //     for (var i = 0; i < listingResults.length; i++) {
-    //         textBooks.push(generateListingUI(listingResults[i]));
-    //     }
-    //     setListedTextbooks(textBooks);
-    // }, [soldOrAvailable, soldOrAvailableIcon])
-
     useEffect(() => {
         // get user textbook listings
         console.log("here")
@@ -190,20 +150,10 @@ function Listings() {
             url: 'https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?puid=' + JSON.parse(localStorage.getItem('userData'))['puid'],
             type: 'GET',
             success: function (result) {
-                localStorage.setItem('userListing', JSON.stringify(result));
                 if (result.length === 0) {
                     list.push(generateNoListing());
-                    // setListedTextbooks(list);
                 } else {
                     for (var i = 0; i < result.length; i++) {
-                        // setListingValues(result[i])
-                        // let listingID = result[i]['listingID']
-                        // let isSold = result[i]['sold']
-                        // setSold(sold => ({
-                        //     ...sold,
-                        //     [listingID]: isSold
-                        // }))
-                        // console.log(sold)
                         list.push(generateListingUI(result[i]));
                     }
                 }
@@ -226,7 +176,7 @@ function Listings() {
             <Box sx={{height: '94%', display: 'flex', flexDirection:'column', alignItems:'center', overFlowY:'scroll'}}>
                 {listedTextbooks}
             </Box>
-            <Dialog open={deleteOpen} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+            <Dialog open={deleteOpen} onClose={deleteClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
                 <DialogTitle id="alert-dialog-title">
                     {"Delete Listing?"}
                 </DialogTitle>
@@ -236,7 +186,7 @@ function Listings() {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={deleteClose}>Cancel</Button>
                     <Button onClick={deleteListing} autoFocus>
                         Delete
                     </Button>
