935aeb77fbc5a953bcd86a6537309b3b89c54d54
x-pham <xpham@purdue.edu>
2022-10-26 16:49:42 -0400

    Add header and placeholder to Watchlist page

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,7 +1,16 @@
-import { Box } from "@mui/material";
+import {
+    Divider, 
+    Box,
+    Typography,
+    CircularProgress  
+} from "@mui/material";
 import './Watchlist.css'
 
 function Watchlist() {
+    /**
+      @todo: add actual ajax request and update loading
+    **/
+    let loading = false;
     return (
         <Box
           sx={{
@@ -11,6 +20,23 @@ function Watchlist() {
             flexDirection: "column",
           }}
         >
+            <Box sx={{ height: "8%" }}>
+                <Typography
+                    variant="h6"
+                    sx={{ fontWeight: "bold", textAlign: "center", padding: "10px" }}
+                >
+                    Your Watchlist
+                </Typography>
+                <Divider
+                    variant="middle"
+                    sx={{ borderBottomColor: "var(--text-color)" }}
+                />
+                {loading ? <CircularProgress/> : 
+                <Typography variant="h6" sx={{ padding: "10px" }}>
+                    No Listings in Watchlist
+                </Typography>
+                }
+            </Box>
         </Box>
       );
 }
