9b651be376057ff4ea557dc26b3889af10770695
x-pham <xpham@purdue.edu>
2022-10-26 19:11:57 -0400

    Implement requesting watchlist from server

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -4,13 +4,34 @@ import {
     Typography,
     CircularProgress  
 } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import $ from "jquery";
 import './Watchlist.css'
 
 function Watchlist() {
+    const [watchlistListings, setWatchlistListings] = useState([]);
+    const [stateChange, setStateChange] = useState(false);
+    const [loading, setLoading] = useState(true);
     /**
       @todo: add actual ajax request and update loading
     **/
-    let loading = false;
+    useEffect(() => {
+        setLoading(true);
+        $.ajax({
+          url:
+            "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/profile?puid=" +
+            JSON.parse(localStorage.getItem("userData"))["puid"],
+          type: "GET",
+          success: function (result) {
+            setLoading(false);
+            setWatchlistListings(result.watchlist);
+          },
+          error: function (result) {
+            console.log(JSON.stringify(result));
+          },
+        });
+      }, [stateChange]);
+
     return (
         <Box
           sx={{
