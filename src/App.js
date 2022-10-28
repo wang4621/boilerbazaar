71529b718d4c27d3887cc274b4d1968a4d0e5d6b
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-10-27 23:10:31 -0400

    added protectedroute

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login"
-import React from "react";
+import React, { useState } from "react";
 import MainPage from "./MainPage";
 import './App.css'
+import ProtectedRoutes from "./component/ProtectedRoutes";
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  console.log(auth)
+
   return (
     <div className="appDisplay">
       <Routes>
-        <Route path="/boilerbazaar" element={<Login />}/>
-        <Route path="/*" element={<MainPage />} />
+        <Route path="/boilerbazaar" element={<Login setAuth={setAuth}/>}/>
+        <Route element={<ProtectedRoutes auth={auth}/>}>
+          <Route path="/*" element={<MainPage />} />
+        </Route>
       </Routes>
     </div>
   );
