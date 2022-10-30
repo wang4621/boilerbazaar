c4b9a3b94f00b29060caec9c2c240b084b3e3641
Michio Sekiguchi <sekiguchimichio02@gmail.com>
2022-10-30 19:35:54 -0400

    BuyListing updated with share and views

--- src/pages/BuyListing.js
+++ src/pages/BuyListing.js
@@ -1,6 +1,12 @@
 import './BuyListing.css'
 import * as React from 'react';
-import { Avatar, Divider, Box, Typography, CardContent, Button } from '@mui/material';
+import { Avatar, Divider, Box, Typography, CardContent, Button, Modal, List, ListItem, ListItemText } from '@mui/material';
+import $ from 'jquery';
+import { useParams } from "react-router-dom";
+import InsertLinkIcon from '@mui/icons-material/InsertLink';
+import IconButton from '@mui/material/IconButton';
+import EmailIcon from '@mui/icons-material/Email';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 // import './SingleListing.css'
 // import * as React from 'react';
@@ -38,7 +44,98 @@ import { Avatar, Divider, Box, Typography, CardContent, Button } from '@mui/mate
 // }
 // export default SingleListing;
 
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
+
+function copyLink() {
+    navigator.clipboard.writeText(window.location.href);
+    alert("Copied to Clipboard");
+
+}
+
 function BuyListing() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const address =  window.location.href;
+    let params = useParams();
+        let id = params.id;
+        const searchUrl = "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/listing?listingID=" + id;
+        $.ajax({
+            url: searchUrl,
+            type: 'GET',    
+            success: function (result) {
+                console.log("success")
+                let listing = result.Items[0];
+                
+                document.getElementById("title").innerHTML = listing.title;
+                document.getElementById("price").innerHTML = "$"+ listing.price;
+                document.getElementById("author").innerHTML = "Author: " + listing.author;
+                //document.getElementById("details").innerHTML = 
+                document.getElementById("isbn").innerHTML = "ISBN: " + listing.isbn;
+                document.getElementById("edition").innerHTML = "Edition: " + listing.edition;
+                document.getElementById("condition").innerHTML = "Condition: " + listing.condition;
+                if (listing.decription != null) {
+                    document.getElementById("decription").innerHTML = "Description: " + listing.decription;
+                }
+                if (listing.views != null) {
+                    document.getElementById("views").innerHTML = listing.views + " views";
+                } else {
+                    document.getElementById("views").innerHTML = 0 + " views";
+                }
+                
+                if (listing.views == null) {
+                    listing.views = 1;
+                } else {
+                    listing.views = listing.views + 1;
+                }
+                
+                // var jsonData = {
+                //     listingID: listing.listingID,
+                //     sellerID: listing.sellerID,
+                //     title: listing.title,
+                //     price: listing.price,
+                //     author: listing.author,
+                //     isbn: listing.isbn,
+                //     edition: listing.edition,
+                //     course: listing.course,
+                //     condition: listing.condition,
+                //     description: listing.description,
+                //     views: listing.views,
+                //   };
+                //   jsonData = '"' + JSON.stringify(jsonData).replaceAll('"', '\\"') + '"';
+                //   $.ajax({
+                //     url: "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/listing",
+                //     type: "PUT",
+                //     data: jsonData,
+                //     datatype: "json",
+                //     contentType: "application/json",
+                //     success: function (result) {
+                //       console.log(JSON.stringify(result));
+                //     },
+                //     error: function (result) {
+                //       console.log(JSON.stringify(result));
+                //     },
+                //   });
+                
+            },
+            error: function (result) {
+                alert(JSON.stringify(result));
+            }
+        });
+
     return (
         <Box sx={{height: '100%', width: '100%', display: 'flex', flexDirection: 'row'}}>
             <Box sx={{ width: '70%', height: '100%', backgroundColor: 'var(--tertiary-color)'}} className="innerLeftBox">
@@ -48,39 +145,78 @@ function BuyListing() {
             </Box>
             <Box sx={{ width: '30%', height: '100%', backgroundColor: 'var(--secondary-color)'}} className="innerRightBox">
                 <CardContent sx={{wordBreak: 'break-word', overflowY: 'scroll', height:'65%', display:'flex', flexDirection:'column'}} className="scrollBar">
-                    <Typography variant="h5" color='var(--text-color)' sx={{fontWeight:'bold'}} id="previewTitle">
+                    <Typography variant="h5" color='var(--text-color)' sx={{fontWeight:'bold'}} id="title">
                         Title
                     </Typography>
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="previewPrice">
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="price">
                         Price
                     </Typography>
                     <br/>
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}}>
+                    {/* <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="details">
                         Details
-                    </Typography>
+                    </Typography> */}
                     <br/>
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}}>
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="author">
                         Author
                         <Typography variant="body1" color='var(--text-color)' id="previewAuthor"/>
                     </Typography>
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}}>
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="isbn">
                         ISBN
                         <Typography variant="body1" color='var(--text-color)' id="previewISBN"/>
                     </Typography>
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}}>
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="edition">
                         Edition
                         <Typography variant="body1" color='var(--text-color)' id="previewEdition"/>
                     </Typography>               
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}}>
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="condition">
                         Condition
                         <Typography variant="body1" color='var(--text-color)' id="previewCondition"/>
                     </Typography>
                     <br/>
-                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}}>
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'bold'}} id="description">
                         Description
                         <Typography variant="body1" color='var(--text-color)' id="previewDescription"/>
                     </Typography>
                     <br/><br/>
+                    <Typography variant="h6" color='var(--text-color)' sx={{fontWeight:'light'}} id="views">
+                        views
+                        <Typography variant="body1" color='var(--text-color)' id="previewCondition"/>
+                    </Typography>
+                    <Button onClick={handleOpen}>Share</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+
+                    >
+                        <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Share Options
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                <IconButton color="inherit" target="_blank" onClick={copyLink} rel="noopener noreferrer">
+                                    <InsertLinkIcon/>
+                                </IconButton>
+                                <ListItemText primary="Copy Link" />
+                            </ListItem>
+                            <ListItem>
+                                <IconButton color="inherit" target="_blank" href={"mailto:?subject=Check out this textbook listing&body=Link to textbook: " + address} rel="noopener noreferrer">
+                                    <EmailIcon/>
+                                </IconButton>
+                                <ListItemText primary="Email" />
+                            </ListItem>
+                            <ListItem>
+                                <div id="fb-root"></div>
+                                <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="eGYQgFrV"></script>
+                                <div data-href={address}></div>
+                                <IconButton color="inherit" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A3000%2Fbuy%2Flisting%2F707647ad-bb0a-41ee-b899-0fbef8c6269c&amp;src=sdkpreparse" >
+                                    <FacebookIcon/>
+                                </IconButton>
+                                <ListItemText primary="Facebook" />
+                            </ListItem>
+                        </List>
+                        </Box>
+                    </Modal>
                 </CardContent>
                 <Divider variant='middle' sx={{borderBottomColor: 'var(--text-color)'}}/>
                 <CardContent sx={{height: '25%', display: 'flex', flexDirection:'column'}}>
