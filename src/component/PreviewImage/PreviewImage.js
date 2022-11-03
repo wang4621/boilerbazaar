2c06b76f690b516b173243042ccf587bc17dc04e
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-11-03 18:12:13 -0400

    css changes

--- src/component/PreviewImage/PreviewImage.js
+++ src/component/PreviewImage/PreviewImage.js
@@ -4,13 +4,25 @@ import CloseIcon from "@mui/icons-material/Close";
 
 const PreviewImage = ({ image, setPreviewImages }) => {
   const deleteImage = (event) => {
-    console.log(event)
+    console.log(event);
     // setPreviewImages();
   };
 
   return (
     <Grid item m={1} xs={5} sx={{ position: "relative" }}>
-      <IconButton sx={{ position: "absolute", zIndex: 1, top: 0, color:"var(--text-color)", backgroundColor:"var(--background-color)"}} onClick={deleteImage}>
+      <IconButton
+        sx={{
+          position: "absolute",
+          zIndex: 1,
+          top: 0,
+          color: "var(--text-color)",
+          backgroundColor: "var(--background-color)",
+          "&.MuiButtonBase-root:hover": {
+            bgcolor: "var(--background-color)",
+          },
+        }}
+        onClick={deleteImage}
+      >
         <CloseIcon />
       </IconButton>
       <img src={image} height={"100%"} width={"100%"} alt="preview" />
