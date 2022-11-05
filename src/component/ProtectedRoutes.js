242c550a2bfc5bf88890af2cfa8216ed670add77
wang4621 <82539201+wang4621@users.noreply.github.com>
2022-11-04 21:03:20 -0400

    navigation fix

--- src/component/ProtectedRoutes.js
+++ src/component/ProtectedRoutes.js
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-const ProtectedRoutes = ({ auth}) => {
+const ProtectedRoutes = ({auth}) => {
     // console.log(auth)
   return auth ? <Outlet /> : <Navigate to="/boilerbazaar" />;
 };
