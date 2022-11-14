6d18137b4015bd713ffe3786978b784aa4846634
fsc1118 <fang282@purdue.edu>
2022-11-14 19:30:48 +0000

    scrapping driver code

---
+++ Scraping/src/server.js
@@ -0,0 +1,28 @@
+// Create a server running on port 8080
+
+// Path: src\server.js
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    const url = req.url.split('?')[0]
+    if (url === '/ebay') {
+        // Get url parameters
+        const urlParams = new URLSearchParams(req.url.split('?')[1])
+        const isbn = urlParams.get('isbn')
+        // Get result by calling script
+        const path = "../script/ebay.py"
+        const result = require('child_process').execSync(`python ${path} ${isbn}`).toString()
+        // Send result
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(result)
+    } else {
+        // Send 404
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+})
+
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080');
+})
--- src/component/ProfileListing/ListingBox.js
+++ src/component/ProfileListing/ListingBox.js
@@ -9,235 +9,235 @@ import EditListing from "./EditListing";
 import $ from "jquery";
 
 const ListingBox = ({ listing, stateChange, setStateChange, userData }) => {
-  let listingSold = listing["sold"];
-  let listingId = listing["listingID"];
+    let listingSold = listing["sold"];
+    let listingId = listing["listingID"];
 
-  const [editOpen, setEditOpen] = useState(false);
-  const [deleteOpen, setDeleteOpen] = useState(false);
-  const [sold, setSold] = useState(listingSold);
-  const [image, setImage] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [editOpen, setEditOpen] = useState(false);
+    const [deleteOpen, setDeleteOpen] = useState(false);
+    const [sold, setSold] = useState(listingSold);
+    const [image, setImage] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  const openDelete = () => {
-    setDeleteOpen(true);
-  };
+    const openDelete = () => {
+        setDeleteOpen(true);
+    };
 
-  const openEdit = () => {
-    setEditOpen(true);
-  };
+    const openEdit = () => {
+        setEditOpen(true);
+    };
 
-  // on button click for "Mark as Sold" and "Mark as Available"
-  const changeTextAndIcon = (event) => {
-    if (event.target.innerText === "Mark as Sold") {
-      // send ajax to update, on success - setSold
-      $.ajax({
-        url:
-          "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?listingID=" +
-          listingId +
-          "&sold=true",
-        type: "PUT",
-        success: function (result) {
-          console.log(JSON.stringify(result));
-          setSold("true");
-          // setStateChange(!stateChange)
-        },
-        error: function (result) {
-          console.log(JSON.stringify(result));
-        },
-      });
-    } else {
-      // send ajax to update, on sucess - setSold
-      $.ajax({
-        url:
-          "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?listingID=" +
-          listingId +
-          "&sold=false",
-        type: "PUT",
-        success: function (result) {
-          console.log(JSON.stringify(result));
-          setSold("false");
-          // setStateChange(!stateChange)
-        },
-        error: function (result) {
-          console.log(JSON.stringify(result));
-        },
-      });
-    }
-  };
+    // on button click for "Mark as Sold" and "Mark as Available"
+    const changeTextAndIcon = (event) => {
+        if (event.target.innerText === "Mark as Sold") {
+            // send ajax to update, on success - setSold
+            $.ajax({
+                url:
+                    "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/transaction" +
+                    listingId +
+                    "&sold=true",
+                type: "PUT",
+                success: function (result) {
+                    console.log(JSON.stringify(result));
+                    setSold("true");
+                    // setStateChange(!stateChange)
+                },
+                error: function (result) {
+                    console.log(JSON.stringify(result));
+                },
+            });
+        } else {
+            // send ajax to update, on sucess - setSold
+            $.ajax({
+                url:
+                    "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/userlisting?listingID=" +
+                    listingId +
+                    "&sold=false",
+                type: "PUT",
+                success: function (result) {
+                    console.log(JSON.stringify(result));
+                    setSold("false");
+                    // setStateChange(!stateChange)
+                },
+                error: function (result) {
+                    console.log(JSON.stringify(result));
+                },
+            });
+        }
+    };
 
-  useEffect(() => {
-    // gets the images for the textbook
-    // console.log(listing["listingID"])
-    setLoading(true);
-    $.ajax({
-      url:
-        "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/listing/images?listingID=" +
-        listing["listingID"],
-      type: "GET",
-      success: function (result) {
-        // console.log(result);
-        setLoading(false);
-        setImage(result["body"][0]);
-      },
-      error: function (result) {
-        console.log(JSON.stringify(result));
-      },
-    });
-  }, [listing]);
+    useEffect(() => {
+        // gets the images for the textbook
+        // console.log(listing["listingID"])
+        setLoading(true);
+        $.ajax({
+            url:
+                "https://66gta0su26.execute-api.us-east-1.amazonaws.com/Prod/listing/images?listingID=" +
+                listing["listingID"],
+            type: "GET",
+            success: function (result) {
+                // console.log(result);
+                setLoading(false);
+                setImage(result["body"][0]);
+            },
+            error: function (result) {
+                console.log(JSON.stringify(result));
+            },
+        });
+    }, [listing]);
 
-  // console.log("listingID:", listingId + " sold: ",listingSold)
-  return (
-    <Grid
-      item
-      m={2}
-      xs={12}
-      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-    >
-      <Box
-        sx={{
-          width: "80%",
-          height: 150,
-          // minHeight: "130px",
-          display: "flex",
-          // flexDirection: "row",
-          // justifyContent: "center",
-          backgroundColor: "var(--secondary-color)",
-          borderRadius: 5,
-          boxShadow: 8,
-        }}
-      >
-        <Box
-          sx={{
-            height: "100%",
-            width: "25%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+    // console.log("listingID:", listingId + " sold: ",listingSold)
+    return (
+        <Grid
+            item
+            m={2}
+            xs={12}
+            sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
         >
-          <Box
-            sx={{
-              height: "80%",
-              width: "80%",
-              backgroundColor: "rgb(32,32,32)",
-              // backgroundColor: "lightgrey",
-              // borderColor:'black',
-              borderRadius: 5,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            {loading ? (
-              <CircularProgress />
-            ) : (
-              <img src={image} height={"100%"} width={"70%"} alt="textbook" />
-            )}
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            height: "100%",
-            width: "75%",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            sx={{
-              height: "80%",
-              width: "95%",
-              display: "flex",
-              flexDirection: "column",
-            }}
-            m={1.5}
-          >
             <Box
-              sx={{
-                height: "80%",
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                // justifyContent: "space-evenly",
-              }}
+                sx={{
+                    width: "80%",
+                    height: 150,
+                    // minHeight: "130px",
+                    display: "flex",
+                    // flexDirection: "row",
+                    // justifyContent: "center",
+                    backgroundColor: "var(--secondary-color)",
+                    borderRadius: 5,
+                    boxShadow: 8,
+                }}
             >
-              <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-                {listing["title"]}
-              </Typography>
-              <Typography variant="body1">${listing["price"]}</Typography>
-              <Typography variant="body2">{listing['currentViews']} clicks on listing</Typography>
-              <Typography variant="body2">
-                Listed on {listing["timeListed"]}
-              </Typography>
+                <Box
+                    sx={{
+                        height: "100%",
+                        width: "25%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            height: "80%",
+                            width: "80%",
+                            backgroundColor: "rgb(32,32,32)",
+                            // backgroundColor: "lightgrey",
+                            // borderColor:'black',
+                            borderRadius: 5,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        {loading ? (
+                            <CircularProgress />
+                        ) : (
+                                <img src={image} height={"100%"} width={"70%"} alt="textbook" />
+                            )}
+                    </Box>
+                </Box>
+                <Box
+                    sx={{
+                        height: "100%",
+                        width: "75%",
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            height: "80%",
+                            width: "95%",
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                        m={1.5}
+                    >
+                        <Box
+                            sx={{
+                                height: "80%",
+                                width: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                // justifyContent: "space-evenly",
+                            }}
+                        >
+                            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                                {listing["title"]}
+                            </Typography>
+                            <Typography variant="body1">${listing["price"]}</Typography>
+                            <Typography variant="body2">{listing['currentViews']} clicks on listing</Typography>
+                            <Typography variant="body2">
+                                Listed on {listing["timeListed"]}
+                            </Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                height: "20%",
+                                width: "100%",
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <Button
+                                variant="contained"
+                                startIcon={sold === "true" ? <CheckIcon /> : <CloseIcon />}
+                                sx={{
+                                    height: "100% !important",
+                                    width: "35%",
+                                    borderRadius: "5px !important",
+                                }}
+                                onClick={changeTextAndIcon}
+                            >
+                                {sold === "true" ? "Mark as Available" : "Mark as Sold"}
+                            </Button>
+                            <Button
+                                variant="contained"
+                                startIcon={<EditIcon />}
+                                sx={{
+                                    height: "100% !important",
+                                    width: "30%",
+                                    borderRadius: "5px !important",
+                                }}
+                                disabled={sold === "true" ? true : false}
+                                onClick={openEdit}
+                            >
+                                Edit Listing
+              </Button>
+                            <Button
+                                variant="contained"
+                                startIcon={<DeleteIcon />}
+                                sx={{
+                                    height: "100% !important",
+                                    width: "30%",
+                                    borderRadius: "5px !important",
+                                }}
+                                onClick={openDelete}
+                            >
+                                Delete Listing
+              </Button>
+                        </Box>
+                    </Box>
+                </Box>
+                <DeleteListing
+                    listingId={listingId}
+                    open={deleteOpen}
+                    setOpen={setDeleteOpen}
+                    stateChange={stateChange}
+                    setStateChange={setStateChange}
+                />
+                <EditListing
+                    listing={listing}
+                    open={editOpen}
+                    setOpen={setEditOpen}
+                    stateChange={stateChange}
+                    setStateChange={setStateChange}
+                    userData={userData}
+                />
             </Box>
-            <Box
-              sx={{
-                height: "20%",
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Button
-                variant="contained"
-                startIcon={sold === "true" ? <CheckIcon /> : <CloseIcon />}
-                sx={{
-                  height: "100% !important",
-                  width: "35%",
-                  borderRadius: "5px !important",
-                }}
-                onClick={changeTextAndIcon}
-              >
-                {sold === "true" ? "Mark as Available" : "Mark as Sold"}
-              </Button>
-              <Button
-                variant="contained"
-                startIcon={<EditIcon />}
-                sx={{
-                  height: "100% !important",
-                  width: "30%",
-                  borderRadius: "5px !important",
-                }}
-                disabled={sold === "true" ? true : false}
-                onClick={openEdit}
-              >
-                Edit Listing
-              </Button>
-              <Button
-                variant="contained"
-                startIcon={<DeleteIcon />}
-                sx={{
-                  height: "100% !important",
-                  width: "30%",
-                  borderRadius: "5px !important",
-                }}
-                onClick={openDelete}
-              >
-                Delete Listing
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-        <DeleteListing
-          listingId={listingId}
-          open={deleteOpen}
-          setOpen={setDeleteOpen}
-          stateChange={stateChange}
-          setStateChange={setStateChange}
-        />
-        <EditListing
-          listing={listing}
-          open={editOpen}
-          setOpen={setEditOpen}
-          stateChange={stateChange}
-          setStateChange={setStateChange}
-          userData={userData}
-        />
-      </Box>
-    </Grid>
-  );
+        </Grid>
+    );
 };
 
 export default ListingBox;
