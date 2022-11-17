403cbb0dd643ff5b46cf65e165021fb118af6c85
fsc1118 <fang282@purdue.edu>
2022-11-17 01:38:36 +0000

    google play

---
+++ Scraping/script/googlePlay.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+# Get arguments from command line
+import sys
+args = sys.argv
+url = "https://play.google.com/store/search?q=isbn&c=books&hl=en_US&gl=US"
+url = url.replace("isbn", args[1])
+# Headers to make the request look more authentic
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+}
+# Scrap https://play.google.com
+page = requests.get(url, headers=headers,)
+# Print the page
+soup = BeautifulSoup(page.content, "html.parser")
+# Get price and url of the books
+# Price is in the span tag under another span tag with class "VfPpfd VixbEe"
+# Url is in the a tag under a div tag with class "WsMG1c nnK0zc"
+
+# Get the price under the span tag
+price = soup.find("span", class_="VfPpfd VixbEe").text
+# Eliminate the dollar sign
+price = price.replace("$", "")
+# Get url which is in the a <a> with class "Si6A0c.ZD8Cqc"
+
+url = soup.find("a", class_="Si6A0c ZD8Cqc").get("href")
+url = "https://play.google.com" + url
+# Print the price and url
+print('{"price": "' + price + '", "url": "' + url + '"}')
--- src/component/ProfileListing/ListingBox.js
+++ src/component/ProfileListing/ListingBox.js
@@ -95,211 +95,152 @@ const ListingBox = ({ listing, stateChange, setStateChange, userData }) => {
             xs={12}
             sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
         >
-<<<<<<< HEAD
-=======
-          <Box
-            sx={{
-              height: "80%",
-              width: "80%",
-              backgroundColor: "rgb(32,32,32)",
-              // backgroundColor: "lightgrey",
-              // borderColor:'black',
-              borderRadius: 5,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            {loading ? (
-              <CircularProgress />
-            ) : (
-              <img src={image} height={"100%"} width={"70%"} alt="textbook" />
-            )}
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            height: "100%",
-            width: "75%",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            sx={{
-              height: "80%",
-              width: "95%",
-              display: "flex",
-              flexDirection: "column",
-            }}
-            m={1.5}
-          >
-            <Box
-              sx={{
-                height: "80%",
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                // justifyContent: "space-evenly",
-              }}
-            >
-              <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-                {listing["title"]}
-              </Typography>
-              <Typography variant="body1">${listing["price"]}</Typography>
-              <Typography variant="body2">
-                {listing["currentViews"]} clicks on listing
-              </Typography>
-              <Typography variant="body2">
-                Listed on {listing["timeListed"]}
-              </Typography>
-            </Box>
->>>>>>> b34d8559bc3cbef2370b696e21bbc98fcbef6f25
-<Box
-    sx={{
-        width: "80%",
-        height: 150,
-        // minHeight: "130px",
-        display: "flex",
-        // flexDirection: "row",
-        // justifyContent: "center",
-        backgroundColor: "var(--secondary-color)",
-        borderRadius: 5,
-        boxShadow: 8,
-    }}
->
-    <Box
-        sx={{
-            height: "100%",
-            width: "25%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-        }}
-    >
-        <Box
-            sx={{
-                height: "80%",
-                width: "80%",
-                backgroundColor: "rgb(32,32,32)",
-                // backgroundColor: "lightgrey",
-                // borderColor:'black',
-                borderRadius: 5,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            {loading ? (
-                <CircularProgress />
-            ) : (
-                    <img src={image} height={"100%"} width={"70%"} alt="textbook" />
-                )}
-        </Box>
-    </Box>
-    <Box
-        sx={{
-            height: "100%",
-            width: "75%",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-        }}
-    >
-        <Box
-            sx={{
-                height: "80%",
-                width: "95%",
-                display: "flex",
-                flexDirection: "column",
-            }}
-            m={1.5}
-        >
             <Box
                 sx={{
-                    height: "80%",
-                    width: "100%",
+                    width: "80%",
+                    height: 150,
+                    // minHeight: "130px",
                     display: "flex",
-                    flexDirection: "column",
-                    // justifyContent: "space-evenly",
+                    // flexDirection: "row",
+                    // justifyContent: "center",
+                    backgroundColor: "var(--secondary-color)",
+                    borderRadius: 5,
+                    boxShadow: 8,
                 }}
             >
-                <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-                    {listing["title"]}
-                </Typography>
-                <Typography variant="body1">${listing["price"]}</Typography>
-                <Typography variant="body2">{listing['currentViews']} clicks on listing</Typography>
-                <Typography variant="body2">
-                    Listed on {listing["timeListed"]}
-                </Typography>
-            </Box>
-            <Box
-                sx={{
-                    height: "20%",
-                    width: "100%",
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                }}
-            >
-                <Button
-                    variant="contained"
-                    startIcon={sold === "true" ? <CheckIcon /> : <CloseIcon />}
+                <Box
                     sx={{
-                        height: "100% !important",
-                        width: "35%",
-                        borderRadius: "5px !important",
+                        height: "100%",
+                        width: "25%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
                     }}
-                    onClick={changeTextAndIcon}
                 >
-                    {sold === "true" ? "Mark as Available" : "Mark as Sold"}
-                </Button>
-                <Button
-                    variant="contained"
-                    startIcon={<EditIcon />}
+                    <Box
+                        sx={{
+                            height: "80%",
+                            width: "80%",
+                            backgroundColor: "rgb(32,32,32)",
+                            // backgroundColor: "lightgrey",
+                            // borderColor:'black',
+                            borderRadius: 5,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        {loading ? (
+                            <CircularProgress />
+                        ) : (
+                                <img src={image} height={"100%"} width={"70%"} alt="textbook" />
+                            )}
+                    </Box>
+                </Box>
+                <Box
                     sx={{
-                        height: "100% !important",
-                        width: "30%",
-                        borderRadius: "5px !important",
+                        height: "100%",
+                        width: "75%",
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
                     }}
-                    disabled={sold === "true" ? true : false}
-                    onClick={openEdit}
                 >
-                    Edit Listing
+                    <Box
+                        sx={{
+                            height: "80%",
+                            width: "95%",
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                        m={1.5}
+                    >
+                        <Box
+                            sx={{
+                                height: "80%",
+                                width: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                // justifyContent: "space-evenly",
+                            }}
+                        >
+                            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                                {listing["title"]}
+                            </Typography>
+                            <Typography variant="body1">${listing["price"]}</Typography>
+                            <Typography variant="body2">
+                                {listing["currentViews"]} clicks on listing
+              </Typography>
+                            <Typography variant="body2">
+                                Listed on {listing["timeListed"]}
+                            </Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                height: "20%",
+                                width: "100%",
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <Button
+                                variant="contained"
+                                startIcon={sold === "true" ? <CheckIcon /> : <CloseIcon />}
+                                sx={{
+                                    height: "100% !important",
+                                    width: "35%",
+                                    borderRadius: "5px !important",
+                                }}
+                                onClick={changeTextAndIcon}
+                            >
+                                {sold === "true" ? "Mark as Available" : "Mark as Sold"}
+                            </Button>
+                            <Button
+                                variant="contained"
+                                startIcon={<EditIcon />}
+                                sx={{
+                                    height: "100% !important",
+                                    width: "30%",
+                                    borderRadius: "5px !important",
+                                }}
+                                disabled={sold === "true" ? true : false}
+                                onClick={openEdit}
+                            >
+                                Edit Listing
               </Button>
-                <Button
-                    variant="contained"
-                    startIcon={<DeleteIcon />}
-                    sx={{
-                        height: "100% !important",
-                        width: "30%",
-                        borderRadius: "5px !important",
-                    }}
-                    onClick={openDelete}
-                >
-                    Delete Listing
+                            <Button
+                                variant="contained"
+                                startIcon={<DeleteIcon />}
+                                sx={{
+                                    height: "100% !important",
+                                    width: "30%",
+                                    borderRadius: "5px !important",
+                                }}
+                                onClick={openDelete}
+                            >
+                                Delete Listing
               </Button>
+                        </Box>
+                    </Box>
+                </Box>
+                <DeleteListing
+                    listingId={listingId}
+                    open={deleteOpen}
+                    setOpen={setDeleteOpen}
+                    stateChange={stateChange}
+                    setStateChange={setStateChange}
+                />
+                <EditListing
+                    listing={listing}
+                    open={editOpen}
+                    setOpen={setEditOpen}
+                    stateChange={stateChange}
+                    setStateChange={setStateChange}
+                    userData={userData}
+                />
             </Box>
-        </Box>
-    </Box>
-    <DeleteListing
-        listingId={listingId}
-        open={deleteOpen}
-        setOpen={setDeleteOpen}
-        stateChange={stateChange}
-        setStateChange={setStateChange}
-    />
-    <EditListing
-        listing={listing}
-        open={editOpen}
-        setOpen={setEditOpen}
-        stateChange={stateChange}
-        setStateChange={setStateChange}
-        userData={userData}
-    />
-</Box>
-        </Grid >
+        </Grid>
     );
 };
 
