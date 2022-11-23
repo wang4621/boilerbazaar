fb6b4404a634bc1926a2b0beabae86cda282a408
jeffreywang <wang4621@purdue.edu>
2022-11-22 21:45:19 -0500

    new home ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,55 @@
-import React, { useEffect, useState } from "react";
-import BrowsingHistory from "../component/BrowsingHistory/BrowsingHistory";
-import SellerRatingPrompt from "../component/Rating/SellerRatingPrompt";
-import $ from "jquery";
+import React from "react";
+import {
+  TextField,
+  Box,
+  Typography,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import RatingstoGive from "../component/Rating/RatingstoGive";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Home.css";
 
 const Home = ({ userData }) => {
-  
-
   return (
     <div className="homeDisplay">
-      {/* <SellerRatingPrompt
-        open={openRating}
-        setOpen={setRatingOpen}
-        stepData={stepData}
-      /> */}
-      <BrowsingHistory />
+      <Box
+        sx={{
+          display: "flex",
+          height: "30%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextField
+          placeholder="Search for users"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          sx={{ mb: 2, width: "60%" }}
+        />
+      </Box>
+      <Box sx={{ display: "flex", height: "70%", flexDirection: "row", bgcolor:'var(--tertiary-color)' }}>
+        <Box
+          sx={{
+            display: "flex",
+            width: "50%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <RatingstoGive userData={userData} />
+        </Box>
+        <Box sx={{ display: "flex", width: "50%" }}>
+          <Typography>Previously Viewed Listings</Typography>
+        </Box>
+      </Box>
     </div>
   );
 };
